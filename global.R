e89fcb2d2ca286b07e55aa9d1bdc3b2e2f1f41c2
James <james.craig@certara.com>
2020-01-21 21:55:57 -0800

    Added helper functions

--- global.R
+++ global.R
@@ -4,6 +4,7 @@ source("lib/libraries.R")
 #source("src/plot_modules.R")
 source("src/data_modules.R")
 source("src/vpc_inputs_modules.R")
+source("src/helper_functions.R")
 #source("src/plotModal.R")
 
 
