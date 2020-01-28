ef5e058824d8d93a73fedb9d9c750f968391021e
James <james.craig@certara.com>
2020-01-27 22:21:17 -0800

    Added footer social links

--- global.R
+++ global.R
@@ -11,6 +11,22 @@ source("src/helper_functions.R")
 
 
 title <- tags$a(href = "https://certara.com",
-                tags$img(src = "certara-shiny.png", height = '35', width = '150'))
+                tags$img(src = "certara-logo.png", height = '34', width = '158'))
+
+youtube <- tags$a(href = "https://youtube.com/user/CertaraLP",
+       tags$img(src = "youtube.png",  height = '34', width = '34'))
+
+facebook <- tags$a(href = "https://facebook.com/CertaraLP",
+                  tags$img(src = "facebook.png", height = '34', width = '34'))
+
+twitter <- tags$a(href = "https://twitter.com/certara",
+                   tags$img(src = "twitter.png", height = '34', width = '34'))
+
+linkedin <- tags$a(href = "https://linkedin.com/company/certara",
+                   tags$img(src = "linkedin.png", height = '34', width = '34'))
+
+
+
+
 
 
